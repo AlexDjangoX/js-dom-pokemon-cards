0b3bd75ba7cc8293a0049c53bb56bf091eefa550
AlexDjangoX <alexmonk17@gmail.com>
2022-04-19 17:45:40 +0200

    basic criteria met

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
+let pokeName;
+let pokeImg;
+let pokeStatName;
+let pokStat;
+let str = ``;
+let strArray = [];
+let htmlString = ``;
+const unorderedList = document.querySelector("ul");
+unorderedList.append(htmlString);
 
-console.log(data);
+console.log(unorderedList);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+for (let j = 0; j < data.length; j++) {
+  pokeName = data[j].name;
+  pokeImg = data[j].sprites.other.dream_world.front_default;
+
+  for (let i = 0; i < data[j].stats.length; i++) {
+    pokeStatName = data[j].stats[i].stat.name.toUpperCase();
+    pokeStat = data[j].stats[i].base_stat;
+    str += `<li>${pokeStatName}: ${pokeStat}<li/>`;
+  }
+
+  htmlString += `<li class="card">
+  <h2 class="card--title">${pokeName}</h2>
+  <img
+    width="256"
+    class="card--img"
+    src="${pokeImg}"
+  />
+  <ul class="card--text">
+    ${str}
+  </ul>
+`;
+
+  str = "";
+}
+
+console.log(htmlString);
+unorderedList.innerHTML = htmlString;
