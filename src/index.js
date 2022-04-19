9e3ae6ae4b10acaeda3b59b6c52cfc62113a1f07
AlexDjangoX <alexmonk17@gmail.com>
2022-04-19 18:25:59 +0200

    redone using createElement() setAttribute() append()

--- src/index.js
+++ src/index.js
@@ -19,21 +19,37 @@ for (let j = 0; j < data.length; j++) {
     pokeStat = data[j].stats[i].base_stat;
     str += `<li>${pokeStatName}: ${pokeStat}<li/>`;
   }
+  let newListItem = document.createElement("li");
+  newListItem.setAttribute("class", "card");
+  unorderedList.append(newListItem);
+  let newHeading = document.createElement("h2");
+  newHeading.setAttribute("class", "card--title");
+  newHeading.innerHTML = pokeName;
+  newListItem.append(newHeading);
+  let newImg = document.createElement("img");
+  newImg.setAttribute("width", "256");
+  newImg.setAttribute("class", "card--img");
+  newImg.setAttribute("src", `${pokeImg}`);
+  newListItem.append(newImg);
+  let newUnordered = document.createElement("ul");
+  newUnordered.setAttribute("class", "card--text");
+  newUnordered.innerHTML = `${str}`;
+  newListItem.append(newUnordered);
 
-  htmlString += `<li class="card">
-  <h2 class="card--title">${pokeName}</h2>
-  <img
-    width="256"
-    class="card--img"
-    src="${pokeImg}"
-  />
-  <ul class="card--text">
-    ${str}
-  </ul>
-`;
+  //   htmlString += `<li class="card">
+  //   <h2 class="card--title">${pokeName}</h2>
+  //   <img
+  //     width="256"
+  //     class="card--img"
+  //     src="${pokeImg}"
+  //   />
+  //   <ul class="card--text">
+  //     ${str}
+  //   </ul>
+  // `;
 
   str = "";
 }
 
 console.log(htmlString);
-unorderedList.innerHTML = htmlString;
+// unorderedList.innerHTML = htmlString;
